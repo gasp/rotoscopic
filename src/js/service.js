0da22b3948396df57f421bd1d22d6f91270d710c
gaspard <gaspard@gmail.com>
2014-11-25 22:09:30 +0100

    service: adding api calls

---
+++ src/js/service.js
@@ -0,0 +1,51 @@
+var service = {};
+service.server = 'http://localhost:3000';
+service.api = 'api';
+service.url = service.server + '/' + service.db;
+
+// load frame
+service.load = function (id, cb) {
+	$.ajax({
+		url: service.url + '/f/' + id,
+		crossDomain: true,
+		type: 'get',
+	}).done(function(data) {
+		if(cb) cb(data);
+		else console.log(data);
+	});
+};
+
+service.save = function(data) {
+	$.ajax({
+		url: service.url + '/f/',
+		crossDomain: true,
+		type: 'put',
+		data:data
+	}).done(function(data) {
+		console.log(data.paths);
+	});
+};
+
+// list project frames
+service.list = function (id, cb) {
+	$.ajax({
+		url: service.url + '/p/f/' + id,
+		crossDomain: true,
+		type: 'get',
+	}).done(function (data) {
+		if(cb) cb(data);
+		else console.log(data);
+	});
+};
+
+
+// list projects
+service.projects = function (cb) {
+	$.ajax({
+		url: service.url + '/p/',
+		crossDomain: true,
+		type: 'get',
+	}).done(function (data) {
+		cb(data);
+	});
+};
